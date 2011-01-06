d34be99a20a2356acdd98c08930a4aa7bc21347e
Mat Trudel <mat@geeky.net>
2011-01-05 21:20:41 -0500

    Reset the description after every method, add arguments to the dsl

--- lib/menutree/leaf.rb
+++ lib/menutree/leaf.rb
@@ -5,19 +5,30 @@ module MenuTree
     def initialize(parent_tree)
       @parent_tree = parent_tree
       @commands = {}
-      @curent_description = ""
+      reset_dsl
     end
 
     def desc(desc)
       @current_description = desc
     end
 
+    def arguments(*arguments)
+      @current_arguments += arguments
+    end
+
     def singleton_method_added(symbol)
       @commands[symbol] = @current_description
+      reset_dsl
     end
 
     def leaf(&block)
       self.instance_exec(@parent_tree, &block) if block_given?
+    end
+
+    private
+    def reset_dsl
+      @current_description = ""
+      @current_arguments = []
     end
   end
 end
